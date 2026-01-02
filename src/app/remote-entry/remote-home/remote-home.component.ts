18ee96ba69ffac5e8e4d47d1288c13a991ccc0c1
Brendon1214 <brooklynwong123@gmail.com>
2026-01-02 11:35:13 +0800

    444

--- src/app/remote-entry/remote-home/remote-home.component.ts
+++ src/app/remote-entry/remote-home/remote-home.component.ts
@@ -8,11 +8,11 @@ import { Router } from '@angular/router';
   
 })
 export class RemoteHomeComponent {
-  private origin = 'http://10.8.0.2:4201'; // ✅ http://你的IP:4201 或你的 remote 域名
+  
+  bannerUrl = `assets/banner-claims.png`;
+  submitUrl = `assets/submit-claim.png`;
+  viewUrl   = `assets/view-claims.png`;
 
-  bannerUrl = `${this.origin}/assets/banner-claims.png`;
-  submitUrl = `${this.origin}/assets/submit-claim.png`;
-  viewUrl   = `${this.origin}/assets/view-claims.png`;
 
   constructor(private router: Router) {}
 
