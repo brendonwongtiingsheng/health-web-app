42ceb45115e81a186f87e1c0a63c89b5a5d01764
Brendon1214 <brooklynwong123@gmail.com>
2026-01-06 15:20:39 +0800

    Add Vercel deployment configuration

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,1 +1,10 @@
-module.exports = require('./webpack.config');
+const config = require('./webpack.config');
+
+module.exports = {
+  ...config,
+  mode: 'production',
+  optimization: {
+    ...config.optimization,
+    minimize: true
+  }
+};
